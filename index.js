80087707a07e5982621497c6017b1c8386287e27
dimblek7 <dimblek7@gmail.com>
2018-06-08 10:36:29 +0530

    update check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send({ hi: "Kiran" });
+    res.send({ hi: "Kiran Dimble" });
 });
 
 const PORT = process.env.PORT || 5000;
